e704ade867317748f8e06e862c6b8e7d3bdf8db9
ING <fjbasualdo@miuandes.cl>
2018-04-10 16:29:45 -0300

    Second commit for april 10 lab

---
+++ empresa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class empresa:clientes
+    {
+        public string permiso;
+
+        public empresa(string micliente, string mipermiso)
+            :base(micliente)
+        {
+            permiso = mipermiso;
+        }
+    }
+}
